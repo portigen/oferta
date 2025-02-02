5d5fea460e7cf1bbfdb9e7b1e3a8a006d8e80668
Tom Sapletta <tom-sapletta-com@users.noreply.github.com>
2025-02-02 18:26:53 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+// config.js
+const config = {
+    isProductPage: false,
+    isComparePage: false,
+    isHomePage: true,
+    maxCompareProducts: 4,
+    apiEndpoint: '/products.json',
+    paths: {
+        compare: '/compare.html',
+        products: '/products'
+    },
+    templates: {
+        productCard: 'templates/product-card.html',
+        productPage: 'templates/product-page.html',
+        comparePage: 'templates/compare-page.html'
+    }
+};
+
+export default config;
